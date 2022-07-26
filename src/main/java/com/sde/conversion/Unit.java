ab9d4804032b54baf949e3e1f017c8601bbbfd99
Jaco Jooste <jaco.jooste@forgerock.com>
2022-07-26 15:32:42 +0100

    Added conversion function

--- src/main/java/com/sde/conversion/Unit.java
+++ src/main/java/com/sde/conversion/Unit.java
@@ -1,14 +1,25 @@
 package com.sde.conversion;
 
+import java.util.function.Function;
+
 public class Unit {
     private final String name;
     private final String abbreviation;
-    private final double conversionConstant;
+    private final Double conversionConstant;
+    private final Function<Object, Double> conversionFunction;
 
     public Unit(String name, String abbreviation, double conversionConstant) {
         this.name = name;
         this.abbreviation = abbreviation;
         this.conversionConstant = conversionConstant;
+        this.conversionFunction = o -> conversionConstant;
+    }
+
+    public Unit(String name, String abbreviation, Function<Object, Double> conversionFunction) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+        this.conversionConstant = null;
+        this.conversionFunction = conversionFunction;
     }
 
     public String getName() {
@@ -20,6 +31,6 @@ public class Unit {
     }
 
     public double getConversionConstant() {
-        return conversionConstant;
+        return conversionConstant == null ? conversionFunction.apply(null) : conversionConstant;
     }
 }
